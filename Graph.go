e98406b564ad109387b37a785d7b9185656e44ac
“zhengyuhao” <zhengyuhaozheng@gmail.com>
2019-12-06 17:46:13 +0800

    添加图的广度优先遍历和深度优先遍历

---
+++ Graph.go
@@ -0,0 +1,128 @@
+package arithmetic
+
+import "fmt"
+
+type  VertexType string   //顶点类型
+type EdgeType int		  //边上的权值
+const Max=^uint16(0)
+var boolBuff []bool
+var quene []int           //宽度优先遍历需要用到的队列
+type  Graph struct {
+	vexs []VertexType 		//节点集合
+	Edges [999][999]EdgeType		//边的邻接矩阵
+	numVexs,numEdgs int		//顶点的个数和边的数量
+}
+
+func creatGraph(g *Graph){
+	fmt.Println("输入你想要建立的图的节点的数量和边的数量")
+	fmt.Scanf("%d%d",&g.numVexs,&g.numEdgs)
+	fmt.Println("请输入每一个节点的数据")
+	for i:=0;i<g.numVexs;i++{
+		term:=""
+		fmt.Scanf("%s",&term)
+		g.vexs=append(g.vexs,VertexType(term))
+	}
+	for i:=0;i<g.numEdgs;i++{
+		for j:=0;j<g.numEdgs;j++{
+			if i==j{
+				g.Edges[i][j]=EdgeType(0)
+			}else{
+				g.Edges[i][j]=EdgeType(Max)
+			}
+		}
+	}
+	fmt.Println(g.numEdgs)
+	fmt.Println("请输入每一条边的数据,分别是起点，终点，长度")
+	for i:=0;i<g.numEdgs;i++{
+		n,m,k:=0,0,0
+		fmt.Scanf("%d%d%d",&n,&m,&k)
+		g.Edges[n][m]=EdgeType(k)
+		//g.Edges[m][n]=EdgeType(k)、、
+	}
+	//DeBug代码，用于检验输入的数据是否正常
+	fmt.Println("图的邻接矩阵")
+	for i:=0;i<g.numVexs;i++{
+		for j:=0;j<g.numVexs;j++{
+			t:=g.Edges[i][j]
+			if t==EdgeType(Max){
+				t=-1
+			}
+			fmt.Printf("%d	",t)
+		}
+		fmt.Println()
+	}
+}
+//深度优先遍历
+func DFS(g *Graph,j int){//g是图，i是遍历的下标
+	if !boolBuff[j]{
+		boolBuff[j]=true
+		fmt.Printf("%v->",g.vexs[j])
+		for i:=0;i<g.numVexs;i++{
+			if g.Edges[j][i]!=EdgeType(Max)&&!boolBuff[i]{
+				DFS(g,i)
+			}
+		}
+	}
+}
+//广度优先遍历
+func BFS(g *Graph){//g是图，i是遍历的下标
+	//入队
+	for i:=0;i<g.numVexs;i++{//遍历每一个节点
+		if !boolBuff[i]{
+			boolBuff[i]=true
+			fmt.Printf("->%v",g.vexs[i])
+			quene=append(quene,i)
+			for len(quene)!=0{
+				x:=quene[0]
+				quene=quene[1:len(quene)]//出队列
+				for j:=0;j<g.numVexs;j++{
+					if g.Edges[x][j]!=EdgeType(Max)&&!boolBuff[j]{
+						boolBuff[j]=true
+						fmt.Printf("->%v",g.vexs[j])
+						quene=append(quene,j)
+						//fmt.Println(quene)
+					}
+				}
+			}
+		}
+	}
+}
+func RunGraph(){
+	g:=new(Graph)
+	creatGraph(g)
+	for i:=0;i<g.numVexs;i++{
+	boolBuff=append(boolBuff,false)
+	}
+
+	//图的深度优先遍历
+	fmt.Println("深度优先遍历")
+	for i:=0;i<g.numVexs;i++{
+		DFS(g,i)
+	}
+	//图的广度优先遍历
+	for i:=0;i<g.numVexs;i++{
+		boolBuff[i]=false
+	}
+	fmt.Println()
+	fmt.Println("广度优先遍历")
+	BFS(g)
+}
+
+/*
+0 1 4
+0 4 2
+0 3 7
+1 5 3
+2 1 9
+2 4 6
+2 3 5
+5 4 1
+ */
+ /*
+ 0
+1
+2
+3
+4
+5
+ */
--- HuffmanTree.go
+++ HuffmanTree.go
@@ -1,1 +1,93 @@
 package arithmetic
+
+import "fmt"
+
+//哈夫曼树的构造以及特性
+
+type huffumanTree struct {
+	weight int
+	parent,left,right int//父节点，左右节点的下标
+}
+
+func getSelect(buff []huffumanTree,end int)(int,int){
+	s1,s2:=0,0
+	min1,min2:=0,0//最小两个数的下标
+	i:=0
+	for buff[i].parent!=-1&&i<=end{
+		i++
+	}
+	min1=buff[i].weight
+	s1=i
+	i++
+	for buff[i].parent!=-1&&i<=end{
+		i++
+	}
+	if buff[i].weight<min1{//第二个节点的权值比第一个小
+		min2=min1
+		s2=s1
+		s1=i
+		min1=buff[i].weight
+	}else{
+		min2=buff[i].weight
+		s2=i
+	}
+	for j:=i+1;j<=end;j++{//遍历之后的找出最小的两个没有父节点的两个节点
+		//如果有父节点的话，直接跳过
+		if buff[j].parent!=-1{
+			continue
+		}
+		//如果比最小的还小
+		if buff[j].weight<min1{
+			min2=min1
+			s2=s1
+			s1=j
+			min1=buff[j].weight
+		}else if buff[j].weight>=min1&&buff[j].weight<min2{
+			min2=buff[j].weight
+			s2=j
+		}
+
+	}
+	return s1,s2
+}
+
+func hffumanTreeShow(start int,buff []huffumanTree){//哈夫曼树的（先序遍历，中序遍历，后序遍历其实否是一样的）
+	if start<0{
+		return
+	}
+	hffumanTreeShow(buff[start].left,buff)
+	hffumanTreeShow(buff[start].right,buff)
+	if buff[start].left==-1&&buff[start].right==-1{
+		fmt.Printf("->%d",buff[start].weight)
+		return
+	}
+}
+func RunHffumanTree(){
+	fmt.Println("输入哈弗曼树的节点的个数，以及每一个节点的权值")
+	n:=0
+	fmt.Scanf("%d",&n)
+	huffBuff:=[]huffumanTree{}
+	//初始化哈夫曼树的数据
+	for i:=0;i<(2*n-1);i++{//给每一个叶子节点赋初始值,不存在单叶子节点的二叉树，所以一共有2*n-1个节点
+		m:=0
+		if i<=(n-1){//叶子节点需要给予权重
+			fmt.Scanf("%d",&m)
+		}
+		t:=new(huffumanTree)
+		t.weight=m
+		t.parent=-1
+		t.left=-1
+		t.right=-1
+		huffBuff=append(huffBuff,*t)
+	}
+	//构造哈夫曼树
+	for i:=n;i<(2*n-1);i++{//之后这里的每一个节点都是新构建的节点
+		x,y:=getSelect(huffBuff,i-1)
+		huffBuff[i].weight=huffBuff[x].weight+huffBuff[y].weight
+		huffBuff[i].left=x
+		huffBuff[i].right=y
+		huffBuff[x].parent,huffBuff[y].parent=i,i
+	}
+	fmt.Println(huffBuff)
+	hffumanTreeShow((2*n-2),huffBuff)
+}
